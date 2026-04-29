0246674feff37847e892bee92334bce21d904127
masterkalajengking <rezaavice@gmail.com>
2026-04-29 07:35:10 +0700

    feat: tambah bottom tab navigation dan membuat struktur folder

---
+++ src/navigation/TabNavigator.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import HomeScreen from '../screens/HomeScreen';
+import FavoritesScreen from '../screens/FavoritesScreen';
+import SearchScreen from '../screens/SearchScreen';
+import AboutScreen from '../screens/AboutScreen';
+
+const Tab = createBottomTabNavigator();
+
+export default function TabNavigator() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Favorit" component={FavoritesScreen} />
+      <Tab.Screen name="Search" component={SearchScreen} />
+      <Tab.Screen name="About" component={AboutScreen} />
+    </Tab.Navigator>
+  );
+}
